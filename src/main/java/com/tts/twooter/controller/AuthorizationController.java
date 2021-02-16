f54c552a1087bf04d84606cfdaa1bfd9360a3b42
cooljoebob64 <cooljoebob64@gmail.com>
2021-02-16 10:40:57 -0600

    Add createNewUser method in AuthorizationController

--- src/main/java/com/tts/twooter/controller/AuthorizationController.java
+++ src/main/java/com/tts/twooter/controller/AuthorizationController.java
@@ -4,7 +4,11 @@ import com.tts.twooter.model.User;
 import com.tts.twooter.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class AuthorizationController {
@@ -15,11 +19,27 @@ public class AuthorizationController {
         this.userService = userService;
     }
 
-    @GetMapping(value="/signup")
+    @GetMapping("/signup")
     public String registration(Model model){
         User user = new User();
         model.addAttribute("user", user);
 
+        return "registration";
+    }
+
+    @PostMapping("/signup")
+    public String createNewUser(@Valid User user,
+                                BindingResult bindingResult,
+                                Model model){
+        User userExists = userService.findByUsername(user.getUsername());
+        if(userExists!=null){
+            bindingResult.rejectValue("username", "error.user", "Username is already taken");
+        }
+        if (!bindingResult.hasErrors()){
+            userService.saveNewUser(user);
+            model.addAttribute("success", "Sign up successful!");
+            model.addAttribute("user", new User());
+        }
         return "registration";
     }
 }
