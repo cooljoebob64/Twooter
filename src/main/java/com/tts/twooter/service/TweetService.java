c1d896cf391031355f41f9aaf12ce778f7736824
Joshua Luppes <cooljoebob64@gmail.com>
2021-02-24 11:35:07 -0600

    Add Tags functionality

--- src/main/java/com/tts/twooter/service/TweetService.java
+++ src/main/java/com/tts/twooter/service/TweetService.java
@@ -9,5 +9,6 @@ public interface TweetService {
     List<Tweet> findAll();
     List<Tweet> findAllByUser(User user);
     List<Tweet> findAllByUsers(List<User> users);
+    List<Tweet> findAllWithTag(String tag);
     void save(Tweet tweet);
 }
--- src/main/java/com/tts/twooter/service/TweetServiceImpl.java
+++ src/main/java/com/tts/twooter/service/TweetServiceImpl.java
@@ -1,38 +1,115 @@
 package com.tts.twooter.service;
 
+import com.tts.twooter.model.Tag;
 import com.tts.twooter.model.Tweet;
 import com.tts.twooter.model.User;
+import com.tts.twooter.repository.TagRepository;
 import com.tts.twooter.repository.TweetRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
-public class TweetServiceImpl implements TweetService{
+public class TweetServiceImpl implements TweetService {
 
     private TweetRepository tweetRepository;
 
-    public TweetServiceImpl(TweetRepository tweetRepository){
-        this.tweetRepository=tweetRepository;
+    private TagRepository tagRepository;
+
+    public TweetServiceImpl(TweetRepository tweetRepository, TagRepository tagRepository) {
+        this.tweetRepository = tweetRepository;
+        this.tagRepository = tagRepository;
     }
 
-    @Override
     public List<Tweet> findAll() {
-        return tweetRepository.findAllByOrderByCreatedAtDesc();
+        List<Tweet> tweets = tweetRepository.findAllByOrderByCreatedAtDesc();
+        return formatTweets(tweets);
     }
 
-    @Override
     public List<Tweet> findAllByUser(User user) {
-        return tweetRepository.findAllByUserOrderByCreatedAtDesc(user);
+        List<Tweet> tweets = tweetRepository.findAllByUserOrderByCreatedAtDesc(user);
+        return tweets;
     }
 
-    @Override
     public List<Tweet> findAllByUsers(List<User> users) {
-        return tweetRepository.findAllByUserInOrderByCreatedAtDesc(users);
+        List<Tweet> tweets = tweetRepository.findAllByUserInOrderByCreatedAtDesc(users);
+        return tweets;
     }
 
-    @Override
+    public List<Tweet> findAllWithTag(String tag) {
+        List<Tweet> tweets = tweetRepository.findByTags_PhraseOrderByCreatedAtDesc(tag);
+        return formatTweets(tweets);
+    }
+
     public void save(Tweet tweet) {
+        handleTags(tweet);
         tweetRepository.save(tweet);
     }
+
+    private void handleTags(Tweet tweet) {
+        List<Tag> tags = new ArrayList<Tag>();
+        Pattern pattern = Pattern.compile("#\\w+");
+        Matcher matcher = pattern.matcher(tweet.getMessage());
+        while (matcher.find()) {
+            String phrase = matcher.group().substring(1).toLowerCase();
+            Tag tag = tagRepository.findByPhrase(phrase);
+            if (tag == null) {
+                tag = new Tag();
+                tag.setPhrase(phrase);
+                tagRepository.save(tag);
+            }
+            tags.add(tag);
+        }
+        tweet.setTags(tags);
+    }
+
+    private List<Tweet> formatTweets(List<Tweet> tweets) {
+        addTagLinks(tweets);
+        shortenLinks(tweets);
+        return tweets;
+    }
+
+    private void addTagLinks(List<Tweet> tweets) {
+        Pattern pattern = Pattern.compile("#\\w+");
+        for (Tweet tweet : tweets) {
+            String message = tweet.getMessage();
+            Matcher matcher = pattern.matcher(message);
+            Set<String> tags = new HashSet<String>();
+            while (matcher.find()) {
+                tags.add(matcher.group());
+            }
+            for (String tag : tags) {
+                message = message.replaceAll(tag,
+                        "<a class=\"tag\" href=\"/tweets/" + tag.substring(1).toLowerCase() + "\">" + tag + "</a>");
+            }
+            tweet.setMessage(message);
+        }
+    }
+
+    private void shortenLinks(List<Tweet> tweets) {
+        Pattern pattern = Pattern.compile("https?[^ ]+");
+        for (Tweet tweet : tweets) {
+            String message = tweet.getMessage();
+            Matcher matcher = pattern.matcher(message);
+            while (matcher.find()) {
+                String link = matcher.group();
+                String shortenedLink = link;
+                if (link.length() > 23) {
+                    shortenedLink = link.substring(0, 20) + "...";
+                    message = message.replace(link,
+                            "<a class=\"tag\" href=\"" + link + "\" target=\"_blank\">" + shortenedLink + "</a>");
+                }
+                tweet.setMessage(message);
+            }
+
+        }
+    }
+
+
+
 }
